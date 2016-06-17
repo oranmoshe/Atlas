6776ca1febc7a1ebb94cf4d7836543b2ab87a62c
Oran <Oran.moshe@gmail.com>
2016-06-17 18:24:21 +0300

    Video info animation bug fixed

--- js/country.js
+++ js/country.js
@@ -6,6 +6,7 @@
   // set main video & color
   var mainVideo;
   var mainColor;
+  var countriesLength=0;
   function setBackgroundVideo(color, src){
     mainVideo = src;
     mainColor = color;
@@ -13,7 +14,9 @@
   }
   // run app
   function run(){
+    var openFlag = 0; 
    $.getJSON( "data/countries.json", function( data ) {
+      countriesLength=data.length;
       $.each( data, function( key, value ) {
 
          $('#countriesList').append('<li id=country'+ key +'><article><div class=title>'+value.title+'</div><div class=country>'+value.country+' ⋆ '+ (key*7+50) +'km</div></article></li>');
@@ -30,7 +33,7 @@
             $("#previews").css('visibility','visible');
          });
 
-         $('#country'+ key + ' div.title').click(function(){
+         $('#country'+ key + ' div.title').on("mousedown",function(){
             var fadeSelector = (!(value.background=='white')?'#fadeBlack':'#fadeWhite');
             var textColor = (!(value.background=='white')?'white':'black');
             $(fadeSelector).fadeIn(500, function() {
@@ -39,11 +42,10 @@
               }else{
                 $("iframe").attr("src",'video.html?vid='+key);
               }
-              $("iframe").css("visibility","visible");
-              $("#iframeHeader").css("visibility","visible");
-<<<<<<< HEAD
               $("#iframeHeader").css("background-color",value.background);
-              $("#iframeHeader span").css("color",textColor);
+              $("#iframeHeader span").css("color",value.color);
+               $("#iframeHeader").addClass("animateClose"); 
+              iframeOpen();
               $(fadeSelector).fadeOut();
             });  
             $("#next").click(function(){
@@ -53,38 +55,31 @@
               goPrev();
             });
             $(".close").click(function(){
-              fadeOut(value.background);
-            });
-            $("#iframeHeader").click(function(event){              
-              if(event.target.id =="iframeHeader") {
-                iframeMenuToggle();
-              }
-            });
-=======
-
-              $(fadeSelector).fadeOut();
-            });
-        });
-
-          $(".close").click(function(){
               var fadeSelector = (!(value.background=='white')?'#fadeBlack':'#fadeWhite');
               $(fadeSelector).fadeIn(500, function() {
-                $("iframe").css("visibility","hidden");
-                $("#iframeHeader").css("visibility","hidden");
-                $("#iframeHeader").removeClass("transparent_block");
+                iframeClose();
                 $(fadeSelector).fadeOut();
-
               });
+            });
+           $("#info").unbind('click').bind('click', function(event){              
+              if(event.target.id =="info") {
+                if(openFlag==0){
+                  iframeMenuOpen();
+                  openFlag=1;
+                  console.log(openFlag);
+                }else{
+                  iframeMenuClose();
+                  openFlag=0;
+                  console.log(openFlag);
+                }
+              }
+            });
           });
->>>>>>> origin/master
-
         });
       });
-    });
-
    var iterator = 0;
    function goNext(){
-    if(iterator<100){
+    if(iterator<countriesLength){
       $("iframe").attr("src",'video.html?vid='+(iterator+1));
         iterator++;
         console.log(iterator);
@@ -97,23 +92,35 @@
         console.log(iterator);
       }
    }
-
    function fadeOut(color){
       var fadeSelector = (!(color=='white')?'#fadeBlack':'#fadeWhite');
       $(fadeSelector).fadeIn(500, function() {
         $("iframe").css("visibility","hidden");
-        $("#iframeHeader").css("visibility","hidden");
-        $("#iframeHeader").removeClass("transparent_block");
         $(fadeSelector).fadeOut();
       });  
    }
-
-   function iframeMenuToggle(){
-        if(!$("#iframeHeader").hasClass("transparent_block")){
-          $("#iframeHeader").addClass("transparent_block");
-        }
-        else{
-          $("#iframeHeader").removeClass("transparent_block");
-        }
+   function iframeOpen(){
+       $("#iframeHeader").css("display","block");
+       $("#iframeHeader").removeClass("hide");
+       $("iframe").removeClass("hide");
    }
+  function iframeClose(){
+       $("iframe").attr("src",'');
+       $("iframe").css("display","none");
+       $("#iframeHeader").addClass("hide");
+       $("#iframeHeader").css("display","none");
+       $("#iframeHeader").removeClass("animateOpen");
+       $("#iframeHeader").removeClass("animateClose");
+   }
+   function iframeMenuOpen(){
+    $("#iframeHeader").removeClass("animateClose");
+    $("#iframeHeader").addClass("animateOpen"); 
+    $("#story").fadeIn(1000);
+   }
+   function iframeMenuClose(){
+    $("#iframeHeader").removeClass("animateOpen");
+    $("#iframeHeader").addClass("animateClose"); 
+    $("#story").fadeOut(300);
+   }
+
 }
